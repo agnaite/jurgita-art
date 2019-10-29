9c7d87ba898058987d3b736107426a06a167d07e
Agne Klimaite <agnaite@me.com>
2019-10-29 12:13:11 -0700

    strip whitespace before save

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,11 +2,15 @@ class Category < ApplicationRecord
   has_many :paintings, dependent: :destroy
 
   validates :name, presence: true
-  before_save :downcase_fields
+  before_save :downcase_fields, :strip_whitespace
 
   private
 
-    def downcase_fields
-      self.name.downcase!
-    end
+  def downcase_fields
+    self.name.downcase!
+  end
+
+  def strip_whitespace
+    self.name.strip
+  end
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -5,18 +5,23 @@ class Painting < ApplicationRecord
   validates :title, :width, :height, :medium, :price, presence: true
   validates :price, :width, :height, numericality: true
   validate :only_one_can_be_checked
-  before_save :downcase_fields
+  before_save :downcase_fields, :strip_whitespace
 
   private
 
-    def only_one_can_be_checked
-      if sold and new
-        errors.add("only one", "label can be checked")
-      end
+  def only_one_can_be_checked
+    if sold and new
+      errors.add("only one", "label can be checked")
     end
+  end
 
-    def downcase_fields
-      self.title.downcase!
-      self.medium.downcase!
-    end
+  def downcase_fields
+    self.title.downcase!
+    self.medium.downcase!
+  end
+
+  def strip_whitespace
+    self.title.strip!
+    self.medium.strip!
+  end
 end
