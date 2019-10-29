4a21759f12cd947409e4e8f04b95c6376125fbfa
Agne Klimaite <agnaite@me.com>
2019-10-29 12:08:06 -0700

    add string validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,11 @@ class Category < ApplicationRecord
   has_many :paintings, dependent: :destroy
 
   validates :name, presence: true
+  before_save :downcase_fields
+
+  private
+
+    def downcase_fields
+      self.name.downcase!
+    end
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -3,10 +3,9 @@ class Painting < ApplicationRecord
   has_one_attached :image
 
   validates :title, :width, :height, :medium, :price, presence: true
-
   validates :price, :width, :height, numericality: true
-
   validate :only_one_can_be_checked
+  before_save :downcase_fields
 
   private
 
@@ -14,5 +13,10 @@ class Painting < ApplicationRecord
       if sold and new
         errors.add("only one", "label can be checked")
       end
+    end
+
+    def downcase_fields
+      self.title.downcase!
+      self.medium.downcase!
     end
 end
