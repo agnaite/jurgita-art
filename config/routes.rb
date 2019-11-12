b74d926e4573aa1671f4cb57b4a6c5ffd14a0aff
Agne Klimaite <agnaite@me.com>
2019-11-11 16:23:37 -0800

    add about and exhibition routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   resources :categories, only: [:show] do
   end
 
+  resources :about, only: [:index] do
+  end
+
+  resources :exhibitions, only: [:index] do
+  end
+
   namespace :admin do
     root "admin#index"
     get :logout
